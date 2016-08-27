53dd9c2c7d139ac234843d4c0250d23e026d2643
wylin <aque9512003@gmailcom>
2016-08-27 10:33:41 -0500

    added helloworldprintf.c

---
+++ helloworldprintf.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+int main(){
+	int fd;
+	mode_t mode=S_IRUSR|S_IWUSR;
+	fd=open("./hello world.txt",O_TRUNC | O_RDWR | O_CREAT,mode);
+	if(fd==-1){
+		perror("Open failed");
+		exit(1);
+	}
+	printf("Hi! My name is Michael.");
+	close(fd);
+	return 0;
+}
