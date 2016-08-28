10b62b4952bc095f48a3d9dc75e334e0d688f073
wylin <aque9512003@gmailcom>
2016-08-28 18:29:28 -0500

    modify programargument.c and Added intaddr.c

---
+++ intaddr.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+	int a[ 10];
+	printf("address of a is %p\n",a);
+	printf("address of a+2 is %p\n",a+2);
+}
--- programargument.c
+++ programargument.c
@@ -1,6 +1,13 @@
 #include <stdlib.h>
+#include <stdio.h>
 
-int main(){
+int main(int argc,char* argv[]){
+	printf("length of argv is %d\n",argc);
+	int count=0;
+	while(*argv++){
+		count++;
+	}
+	printf("length of argv is %d\n",count);
 
 	return 0;
 }
